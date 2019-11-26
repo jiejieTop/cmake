430cf60a24509538631bdade2fef44defce1ff95
jiejietop <1161959934@qq.com>
2019-11-27 00:22:28 +0800

    1. add section1: single file 2. add section2: build folder compilation 3. add section3: multi file compilation

---
+++ section1/main.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: jiejie
+ * @Github: https://github.com/jiejieTop
+ * @Date: 2019-11-26 23:05:18
+ * @LastEditTime: 2019-11-26 23:06:04
+ * @Description: the code belongs to jiejie, please keep the author information and source code according to the license.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ section3/math.c
@@ -0,0 +1,23 @@
+/*
+ * @Author: jiejie
+ * @Github: https://github.com/jiejieTop
+ * @Date: 2019-11-27 00:11:33
+ * @LastEditTime: 2019-11-27 00:14:29
+ * @Description: the code belongs to jiejie, please keep the author information and source code according to the license.
+ */
+
+double power(double base, int exponent)
+{
+    int result = base;
+    int i;
+
+    if (exponent == 0) {
+        return 1;
+    }
+    
+    for(i = 1; i < exponent; ++i){
+        result = result * base;
+    }
+
+    return result;
+}

02c930faa890d3f00265391a39c6e3a2f35b98a5
jiejie <1161959934@qq.com>
2019-11-29 09:51:29 +0000

    add section5

---
+++ section5/lib/power/power.h
@@ -0,0 +1,14 @@
+/*
+ * @Author: jiejie
+ * @Github: https://github.com/jiejieTop
+ * @Date: 2019-11-27 00:11:51
+ * @LastEditTime: 2019-11-27 00:14:51
+ * @Description: the code belongs to jiejie, please keep the author information and source code according to the license.
+ */
+#ifndef POWER_H
+#define POWER_H
+
+double power(double base, int exponent);
+
+#endif
+
---
+++ section5/src/main.c
@@ -0,0 +1,23 @@
+/*
+ * @Author: jiejie
+ * @Github: https://github.com/jiejieTop
+ * @Date: 2019-11-26 23:54:34
+ * @LastEditTime: 2019-11-29 00:54:51
+ * @Description: the code belongs to jiejie, please keep the author information and source code according to the license.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "power/power.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3){
+        printf("Usage: %s base exponent \n", argv[0]);
+        return 1;
+    }
+    double base = atof(argv[1]);
+    int exponent = atoi(argv[2]);
+    double result = power(base, exponent);
+    printf("%g ^ %d is %g\n", base, exponent, result);
+    return 0;
+}
